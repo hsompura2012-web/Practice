b1948c02dc38d96a0c3528c6f1f1d4f002fb3ebd
Harshit <hsompura2012@gmail.com>
2025-11-26 11:17:29 +0530

    Update

--- SetAnsible.sh
+++ SetAnsible.sh
@@ -1,2 +1,2 @@
 git pull
-ansible-playbook -i {$1}-dev.harshitsompura.online, {$1}.yaml -e ansible_user=ec2-user -e ansible_password=DevOps321
+ansible-playbook -i $1-dev.harshitsompura.online, $1.yaml -e ansible_user=ec2-user -e ansible_password=DevOps321
