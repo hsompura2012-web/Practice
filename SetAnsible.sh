80b7d9a34782a63708974a2e58dd3d30c2ad64e8
Harshit <hsompura2012@gmail.com>
2025-11-26 14:49:43 +0530

    Update

--- SetAnsible.sh
+++ SetAnsible.sh
@@ -1,2 +1,7 @@
 git pull
+if [ -z $1 ]; then
+  echo "input is expected"
+  exit 1
+fi
+
 ansible-playbook -i $1-dev.harshitsompura.online, $1.yaml -e ansible_user=ec2-user -e ansible_password=DevOps321
